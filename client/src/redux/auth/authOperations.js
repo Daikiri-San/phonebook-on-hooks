f1b0fa1da6f003c8844f820a65c6c6f19c30147b
Nikita Holovnin <daikirisan@gmail.com>
2020-11-09 22:58:09 +0200

    change axios baseUrl

--- client/src/redux/auth/authOperations.js
+++ client/src/redux/auth/authOperations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authActions from './authActions';
 
-axios.defaults.baseURL = 'http://localhost:8080/';
+axios.defaults.baseURL = 'https://daikiri-phonebook-app.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 const token = {
