7e0a75c923d68f27260c11cebdec85ddf747968e
Nikita Holovnin <daikirisan@gmail.com>
2020-11-10 14:16:59 +0200

    contacts config

--- client/src/redux/contacts/contactsSelectors.js
+++ client/src/redux/contacts/contactsSelectors.js
@@ -8,14 +8,11 @@ const getError = ({ contacts }) => contacts.error;
 const getVisibleContacts = createSelector(
   [getContacts, getFilter],
   (contacts, filter) => {
-    if (Array.isArray(contacts) && contacts.length > 0) {
-      return contacts.filter(
-        ({ name, number }) =>
-          name.toLowerCase().includes(filter.toLowerCase()) ||
-          number.includes(filter),
-      );
-    }
-    return;
+    return contacts.filter(
+      ({ name, number }) =>
+        name.toLowerCase().includes(filter.toLowerCase()) ||
+        number.includes(filter),
+    );
   },
 );
 
